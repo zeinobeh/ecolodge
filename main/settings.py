600bf9d83a4ddc04fa7117860c5f1b68f6c7cdcd
zeinobeh <zeinobeh@gmail.com>
2026-09-07 20:37:06 +0330

    add default value to secret key

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# خط قبلی را پاک کنید و این را بنویسید:
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-temporary-key-for-render')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False, cast=bool)
