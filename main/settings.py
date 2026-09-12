bebadd866a87168eb7102602470876dbdcc596bb
zeinobeh <zeinobeh@gmail.com>
2026-09-13 00:01:36 +0330

    Debug=true

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-temporary-key-for-ren
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
